c6243ffc42c043f532dec77c0d2fc048c5a9656b
KevSeenan <kev.seenan@gmail.com>
2019-08-27 17:35:57 +0100

    Adds txet box for entering amount and drop down of countries

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
-import Vue from 'vue'
+import Vue from 'vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
-    el: '#app'
-  });
+    el: '#app',
+    data: {
+      currencies: {},
+      selectedCurrency: null,
+    },
+    mounted() {
+      this.fetchCurrencies();
+    },
+    methods: {
+      fetchCurrencies: function () {
+        fetch("https://api.exchangeratesapi.io/latest")
+        .then(result => result.json())
+        .then(data => this.currencies = data.rates)
+      }
+    }
+  })
 })
